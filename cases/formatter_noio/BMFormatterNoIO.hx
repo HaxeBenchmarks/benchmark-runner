85420242c9445463da475a50d12ee03d6c4df73c
AlexHaxe <Alexander.Blum@gmail.com>
2020-05-04 20:30:24 +0200

    added formatter NoIO benchmark

--- cases/formatter_noio/BMFormatterNoIO.hx
+++ cases/formatter_noio/BMFormatterNoIO.hx
@@ -1,28 +1,13 @@
 import sys.FileSystem;
 import benchmark.Benchmark;
 
-class BMFormatter {
+class BMFormatterNoIO {
 	public static function main():Void {
-		var dataSources = [
-			{dir: "haxe", url: "https://github.com/HaxeFoundation/haxe.git", sub: "std"},
-			{dir: "openfl", url: "https://github.com/openfl/openfl.git", sub: "src"},
-			{dir: "lime", url: "https://github.com/openfl/lime.git", sub: "src"}
-		];
 		FileSystem.createDirectory("data");
-		var args = [];
-		for (source in dataSources) {
-			args.push("-s");
-			args.push('../data/${source.dir}/${source.sub}');
-			// don't check out fresh sources automatically
-			// should be a manual, because new sources will have an effect on benchmark runtime
-			// 	if (!FileSystem.exists('data/${source.dir}'))
-			// 		Sys.command('git clone ${source.url} --depth 1 data/${source.dir}');
-		}
 		Benchmark.benchmarkAll(
 			// version setup
 			(haxe) -> {
 				installLibraries: haxe == "haxe3" ? [
-					"benchmark-helper" => "gh://github.com/HaxeBenchmarks/benchmark-helper#90093c51193bf621a6a02910d45a341cef97820e",
 					"formatter" => "haxelib:/formatter#1.9.1",
 					"haxeparser" => "gh://github.com/Simn/haxeparser#48160b190cacafb0003d0c8d085dca2c85e21e31",
 					"hxargs" => "haxelib:/hxargs#3.0.2",
@@ -31,7 +16,6 @@ class BMFormatter {
 					"json2object" => "haxelib:/json2object#3.6.4",
 					"tokentree" => "haxelib:/tokentree#1.0.23"
 				] : [
-					"benchmark-helper" => "gh://github.com/HaxeBenchmarks/benchmark-helper#90093c51193bf621a6a02910d45a341cef97820e",
 					"formatter" => "haxelib:/formatter#1.9.1",
 					"haxeparser" => "gh://github.com/Simn/haxeparser#e5746bfc55c09a3694db42738ff575b91441971a",
 					"hx3compat" => "haxelib:/hx3compat#1.0.3",
@@ -50,21 +34,14 @@ class BMFormatter {
 					"hxparse",
 					"json2object",
 					"hxargs",
-					"formatter",
-					"benchmark-helper"
+					"formatter"
 				],
-				main: "formatter.Cli"
+				classPaths: [".."],
+				main: "BMFormatterNoIOCode"
 			},
 			// target run
 			(haxe, target) -> {
-				var cwd = Sys.getCwd();
-				for (source in dataSources) {
-					Sys.setCwd('../data/${source.dir}');
-					Sys.command('git checkout -- .');
-					Sys.setCwd(cwd);
-				}
 				{
-					args: args,
 					timeout: 5 * 60
 				};
 			}
---
+++ cases/formatter_noio/BMFormatterNoIOCode.hx
@@ -0,0 +1,52 @@
+import haxe.Timer;
+import haxe.io.Path;
+import sys.FileSystem;
+import sys.io.File;
+import formatter.Formatter;
+import formatter.config.Config;
+import tokentree.TokenTreeBuilder.TokenTreeEntryPoint;
+
+class BMFormatterNoIOCode {
+	public function new() { 
+		// segmentation faults in C++
+		// var sources:Array<String> = BenchmarkStatMacro.getSources("dataNoIO");
+
+		var sources:Array<String> = getSources("../data");
+		var config:Config = new Config();
+		var config2:Config = new Config();
+		config2.lineEnds.leftCurly = Both;
+
+		for (i in 0...30) {
+			for (src in sources) {
+				Formatter.format(Code(src), config, null, TokenTreeEntryPoint.TYPE_LEVEL);
+				Formatter.format(Code(src), config2, null, TokenTreeEntryPoint.TYPE_LEVEL);
+			}
+		}
+	}
+
+	function getSources(path:String):Array<String> {
+		var sources:Array<String> = [];
+
+		var folderFunc:String->Void;
+		folderFunc = function(path:String):Void {
+			for (f in FileSystem.readDirectory(path)) {
+				var fileName:String = Path.join([path, f]);
+				if (FileSystem.isDirectory(fileName)) {
+					folderFunc(fileName);
+					continue;
+				}
+				sources.push(File.getContent(fileName));
+			}
+		}
+		folderFunc(path);
+		return sources;
+	}
+
+	static function main() {
+		try {
+			new BMFormatterNoIOCode();
+		} catch (e:Any) {
+			trace(e);
+		}
+	}
+}
