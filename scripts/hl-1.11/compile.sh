f3a6f25aa01a66670146aae1f332b635f1910264
AlexHaxe <Alexander.Blum@gmail.com>
2020-06-12 23:29:27 +0200

    added -lm to hlc compile.sh

--- scripts/hl-1.11/compile.sh
+++ scripts/hl-1.11/compile.sh
@@ -4,4 +4,4 @@ target="$1"
 
 readonly HASHLINK_BASE="$TOOLING_BASE/hashlink/hashlink-1.11"
 
-gcc -O3 -std=c11 -o "$target/hlc" "$target/hlc.c" -I "$HASHLINK_BASE/include" -I "$target" -L "$HASHLINK_BASE/lib" -lhl "$HASHLINK_BASE/lib/fmt.hdll"
+gcc -O3 -std=c11 -o "$target/hlc" "$target/hlc.c" -I "$HASHLINK_BASE/include" -I "$target" -L "$HASHLINK_BASE/lib" -lhl -lm "$HASHLINK_BASE/lib/fmt.hdll"
