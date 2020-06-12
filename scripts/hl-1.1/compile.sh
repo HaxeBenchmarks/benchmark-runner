cf411d0855be76274a62eb827bda2837f911d5c8
Aurel Bílý <aurel.bily@gmail.com>
2020-06-12 13:00:28 +0100

    add fmt.hdll to HL compile scripts

--- scripts/hl-1.1/compile.sh
+++ scripts/hl-1.1/compile.sh
@@ -4,4 +4,4 @@ target="$1"
 
 readonly HASHLINK_BASE="$TOOLING_BASE/hashlink/hashlink-1.1"
 
-gcc -m32 -O3 -std=c11 -o "$target/hlc" "$target/hlc.c" -I "$HASHLINK_BASE/include" -I "$target" -L "$HASHLINK_BASE/lib" -lhl
+gcc -m32 -O3 -std=c11 -o "$target/hlc" "$target/hlc.c" -I "$HASHLINK_BASE/include" -I "$target" -L "$HASHLINK_BASE/lib" -lhl "$HASHLINK_BASE/lib/fmt.hdll"
